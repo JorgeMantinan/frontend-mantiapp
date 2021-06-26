ebd66cb0fbba038fde207f7db0703723d0aa9d9d
JorgeMantinan <jorgemantinanmontero@gmail.com>
2021-06-26 18:48:44 +0200

    Creation of ownership component

---
+++ src/app/ownership/ownership.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ownership',
+  templateUrl: './ownership.component.html',
+  styleUrls: ['./ownership.component.sass']
+})
+export class OwnershipComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
