cd14d3ecd73065a8cd0d91e97185b23825467fc6
JorgeMantinan <jorgemantinanmontero@gmail.com>
2021-06-26 18:30:02 +0200

    Creation of header component

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.sass']
+})
+export class HeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
