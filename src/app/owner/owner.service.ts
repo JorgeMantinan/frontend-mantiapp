1e1a2925c92d298f7a4b020f69190e435cba53b5
JorgeMantinan <jorgemantinanmontero@gmail.com>
2021-07-08 20:03:27 +0200

    HttpClient obtained Owners from apiRest

--- src/app/owner/owner.service.ts
+++ src/app/owner/owner.service.ts
@@ -4,15 +4,21 @@ import { OWNERS } from './owner.json';
 // Is used for make objects string for use like Observable manage for big data
 // Observable is for change so fast the data
 import { Observable, of } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OwnerService {
 
-  constructor() { }
+  private urlEndPoint: string = 'http://localhost:8080/api/owners';
 
-  getOwners(): Observable <Owner[]> { 
-    return of(OWNERS);
+  constructor(private http: HttpClient) { }
+
+  getOwners(): Observable <Owner[]> {
+    return this.http.get(this.urlEndPoint).pipe(
+      map( response => response as Owner[])
+    );
   }
 }
